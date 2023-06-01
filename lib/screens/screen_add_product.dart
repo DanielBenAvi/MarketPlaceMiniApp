cf1c1c1be910168fb25d9183c9ba37275faccc5b
daniel ben avi <ben.avi.daniel@s.afeka.ac.il>
2023-06-01 22:50:43 +0300

    update

--- lib/screens/screen_add_product.dart
+++ lib/screens/screen_add_product.dart
@@ -7,6 +7,12 @@ import 'dart:typed_data';
 import 'package:currency_picker/currency_picker.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:marketplace/api/object_api.dart';
+import 'package:marketplace/api/user_api.dart';
+import 'package:marketplace/other/item_object.dart';
+import 'package:marketplace/other/preferences.dart';
+import 'package:marketplace/singleton_user.dart';
+import 'package:marketplace/widgets/multi_select_dialog.dart';
 
 class ScreenAddProduct extends StatefulWidget {
   const ScreenAddProduct({Key? key}) : super(key: key);
@@ -21,6 +27,22 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
   PlatformFile? pickedFile;
   UploadTask? uploadTask;
   String? downloadURL;
+  List<ItemObject> _selectedPreferences = [];
+
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _priceController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateRole();
+  }
+
+  Future updateRole() async {
+    await UserApi().updateRole('SUPERAPP_USER');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +61,7 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
                 SizedBox(
                   width: 300,
                   child: TextFormField(
+                    controller: _nameController,
                     decoration: const InputDecoration(
                       labelText: 'Name',
                       hintText: 'Enter your product name',
@@ -56,6 +79,7 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
                 SizedBox(
                   width: 300,
                   child: TextFormField(
+                    controller: _descriptionController,
                     maxLines: 5,
                     decoration: const InputDecoration(
                       labelText: 'Description',
@@ -74,6 +98,8 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
                 SizedBox(
                   width: 300,
                   child: TextFormField(
+                    controller: _priceController,
+                    keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Price',
@@ -98,7 +124,15 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
                 OutlinedButton(
                     onPressed: _filePiker, child: const Text('Add Image')),
                 // todo: add preference picker
-
+                const SizedBox(height: 20),
+                MultiSelect(
+                  "Preferences",
+                  "Preferences",
+                  Preferences().getPreferences(),
+                  onMultiSelectConfirm: (List<ItemObject> results) {
+                    _selectedPreferences = results;
+                  },
+                ),
                 const SizedBox(height: 20),
                 SizedBox(
                   width: 300,
@@ -110,6 +144,7 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
                             content: Text('Processing Data'),
                           ),
                         );
+                        _addProduct();
                       }
                     },
                     child: const Text('Submit'),
@@ -158,7 +193,10 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
   }
 
   Future _filePiker() async {
-    final result = await FilePicker.platform.pickFiles();
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['png', 'jpeg'],
+    );
     if (result == null) return;
 
     setState(() {
@@ -175,5 +213,46 @@ class _ScreenAddProductState extends State<ScreenAddProduct> {
       downloadURL = urlDownload;
     });
     debugPrint('Download-Link: $urlDownload');
+  }
+
+  Future _addProduct() async {
+    SingletonUser singletonUser = SingletonUser.instance;
+    Map<String, dynamic> object = {
+      "objectId": {},
+      "type": "PRODUCT",
+      "alias": "product",
+      "active": true,
+      "location": {"lat": 10.200, "lng": 10.200},
+      "createdBy": {
+        "userId": {
+          "superapp": "2023b.LiorAriely",
+          "email": singletonUser.email,
+        }
+      },
+      "objectDetails": {
+        "name": _nameController.text,
+        "description": _descriptionController.text,
+        "price": double.parse(_priceController.text),
+        "currency": currencyCode,
+        "image": downloadURL,
+        "status": "AVAILABLE",
+        "preferences": _selectedPreferences.map((e) => e.name).toList(),
+      }
+    };
+
+    ObjectApi().postObjectJson(object).then((value) {
+      debugPrint('object posted');
+    }).catchError(
+      // ignore: invalid_return_type_for_catch_error
+      (error) => debugPrint('error: $error'),
+    );
+
+    _screenExploreProducts();
+  }
+
+  void _screenExploreProducts() {
+    // pop all the screens
+    Navigator.popUntil(context, (route) => route.isFirst);
+    Navigator.pushNamed(context, '/explore_products');
   }
 }
